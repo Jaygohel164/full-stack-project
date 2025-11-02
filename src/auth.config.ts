dc327baafac59ed30cc8ebe637fb2ffb19279bc4
Jaygohel164 <jaygohel164@gmail.com>
2025-11-02 21:04:28 +0530

    auth edited

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,10 +1,10 @@
 import bcrypt from "bcryptjs";
-import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { NextAuthConfig } from "next-auth/lib";
 
 const authConfig = {
   providers: [
